98e5e8b4d11c5c22bccb433e852e9861ce52d22a
Jeickson <jeick@hotmail.com>
2017-03-16 19:57:08 +0100

    BioLife 1.0 sin errores

---
+++ web/js/app.js
@@ -0,0 +1,10 @@
+// jQuery code
+$(document).ready(function () {
+
+});
+
+// Angular code
+(function(){
+  var cookiesApp = angular.module('cookiesApp', ['ng-currency', 'ui.bootstrap', 'ngCookies', 'angularUtils.directives.dirPagination']);
+
+})();
