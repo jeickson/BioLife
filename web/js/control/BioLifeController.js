f181c8ae809b1e61ca72600311a0632ffc4fcc43
alumne <alumne@alumne>
2017-05-26 14:09:51 +0200

    Articles View Finished with filter.

--- web/js/control/BioLifeController.js
+++ web/js/control/BioLifeController.js
@@ -10,17 +10,18 @@
 angular.module("BioLifeApp").controller("BioLifeController",['$scope', '$window', '$cookies','$filter',function ($scope, $window, $cookies,$filter){
       $scope.actionView='main';
       $scope.loginView=false;
-      
-        
+      $scope.filterAction;
+
+
        $scope.loginOpenClose=function(){
-            
+
             if(!$scope.loginView){
                 $scope.loginView=true;
                 $('#divFullContent').addClass('col-sm-9 col-md-9 col-lg-9');
                 $('#logo').hide();
                 $('#iconsHeaderDiv').removeClass('col-lg-4');
-               
-                
+
+
             }else{
                 $scope.loginView=false;
                 $('#divFullContent').removeClass('col-sm-9 col-md-9 col-lg-9');
